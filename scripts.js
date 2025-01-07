6be122b9209c029f16f52c52bb55b572a847c1a3
Cecilia Romero <96091555+MariaCeciliaRomero@users.noreply.github.com>
2025-01-06 22:22:38 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,12 @@
-// Función para el desplazamiento suave hacia las secciones
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.addEventListener("DOMContentLoaded", function() {
+    const titulo = document.getElementById("titulo");
+    console.log(titulo);  // Esto debería imprimir el elemento
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+    titulo.addEventListener("click", function() {
+        titulo.style.backgroundColor = "yellow";
+        titulo.style.color = "blue";
+        console.log("¡Se ha hecho clic en el título!");
     });
-});
-
-// Obtener el elemento por su id
-const titulo = document.getElementById("titulo");
-
-// Función para cambiar el estilo del título con clic
-titulo.addEventListener("click", function() {
-    // Cambiar el color de fondo y el color del texto
-    titulo.style.backgroundColor = "yellow";
-    titulo.style.color = "blue";
 });
 
 

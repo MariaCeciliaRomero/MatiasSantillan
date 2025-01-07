69fb88b15a5dd77ff856d0c3d6b59d783caa52e5
Cecilia Romero <96091555+MariaCeciliaRomero@users.noreply.github.com>
2025-01-06 22:41:16 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     const titulo = document.getElementById("titulo");
-    console.log(titulo);  // Esto debería imprimir el elemento
 
-    // Añadimos un evento de clic para activar la animación
-    titulo.addEventListener("click", function() {
-        // Activamos el efecto de máquina de escribir
-        titulo.style.animation = "typing 3s steps(30) 1s 1 normal both, blinkCaret 0.75s step-end infinite";
-        console.log("¡Se ha hecho clic en el título!");
-    });
+    // Aquí puedes añadir otras interacciones si lo deseas, o dejarlo vacío
 });
+
 
 
 

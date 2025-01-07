5257b6e5aca7dcf0247301ca6af44f714889a68c
Cecilia Romero <96091555+MariaCeciliaRomero@users.noreply.github.com>
2025-01-06 21:42:54 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,15 +16,19 @@ document.addEventListener("DOMContentLoaded", function() {
     let index = 0;
     const titulo = document.getElementById("titulo");
 
-    // Función para escribir el texto lentamente
-    function escribirTexto() {
-        if (index < texto.length) {
-            titulo.innerHTML += texto.charAt(index);
-            index++;
-            setTimeout(escribirTexto, 100); // Tiempo entre cada letra
+    // Verificamos si el título está disponible
+    if (titulo) {
+        // Función para escribir el texto lentamente
+        function escribirTexto() {
+            if (index < texto.length) {
+                titulo.innerHTML += texto.charAt(index);
+                index++;
+                setTimeout(escribirTexto, 100); // Tiempo entre cada letra
+            }
         }
+
+        escribirTexto();
+    } else {
+        console.error("No se encontró el elemento con id 'titulo'.");
     }
-
-    escribirTexto();
 });
-

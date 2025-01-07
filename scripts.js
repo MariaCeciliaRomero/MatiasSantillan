eb1690cca262c78e6f62f880811cf006807f494c
Cecilia Romero <96091555+MariaCeciliaRomero@users.noreply.github.com>
2025-01-06 22:25:39 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,11 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const titulo = document.getElementById("titulo");
     console.log(titulo);  // Esto debería imprimir el elemento
 
+    // Añadimos un evento de clic para activar la animación
     titulo.addEventListener("click", function() {
-        titulo.style.backgroundColor = "yellow";
-        titulo.style.color = "blue";
+        // Activamos el efecto de máquina de escribir
+        titulo.style.animation = "typing 3s steps(30) 1s 1 normal both, blinkCaret 0.75s step-end infinite";
         console.log("¡Se ha hecho clic en el título!");
     });
 });
+
 
 
